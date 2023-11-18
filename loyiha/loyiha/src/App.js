0d76291211a6617e46b833fe7324a2ec92a29e86
Abdumumin24 <alimovabdu70@gmail.com>
2023-11-18 12:18:17 +0500

    Edumark

---
+++ loyiha/loyiha/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './pages/navbar';
+import Slider from './pages/slider';
+import AboutArea from './pages/about_area';
+import Popular from './pages/popular';
+import Testimonial from './pages/testimonial';
+import Course from './pages/course';
+import Newsletter from './pages/newsletter';
+import LatestBlog from './pages/latest_blog';
+import Footer from './pages/footer';
+import FormItself from './pages/form_itself';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Slider/>
+      <AboutArea/>
+      <Popular/>
+      <Testimonial/>
+      <Course/>
+      <Newsletter/>
+      <LatestBlog/>
+      <Footer/>
+      <FormItself/>
+    </div>
+  );
+}
+
+export default App;
